ac34df9969a6a50a04ff6bec5941f998210f18c9
Stygus <kuba.tokarczyk@onet.eu>
2024-11-21 19:47:29 +0100

    Dodanie metody preorder

--- Drzewo BST/Drzewo BST/Drzewo.cpp
+++ Drzewo BST/Drzewo BST/Drzewo.cpp
@@ -124,8 +124,6 @@ void Drzewo::usunDrzewo() {};
 
 
 
-
-
 elementDrzewa* Drzewo::szukajElementu(int wartosc) {
 	elementDrzewa* temp = korzen;
 
@@ -157,26 +155,50 @@ elementDrzewa* Drzewo::szukajElementu(int wartosc) {
 
 
 
+void preorder(elementDrzewa* element) {
+	if (!element) return;
+	
+	
+	cout << element->wartosc << " ";
+	preorder(element->lewy);
+	preorder(element->prawy);
+}
+
+
+void postorder(elementDrzewa* element) {
+	if (!element) return;
+
+	postorder(element->lewy);
+	postorder(element->prawy);
+	cout << element->wartosc << " ";
+}
 
 
 void Drzewo::wyswietlDrzewo() {
 	int komenda = 0;
 	elementDrzewa temp = *korzen;
-
 	cout << "0 - preorder, 1 - inorder, 2 - postorder\n>  ";
 	cin >> komenda;
 	switch (komenda)
 	{
 	case 0:
-		while (true) {
-			
-		}
+		preorder(korzen);
+		break;
+	case 1:
+		
+		break;
+	case 2:
+
 		break;
 
 	default:
 		break;
 	}
 };
+
+
+
+
 void Drzewo::zapiszDoPliku() {
 };
 void Drzewo::wczytajZPliku() {
